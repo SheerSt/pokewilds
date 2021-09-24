8d78cc95a1e399a9c276f386a52c13309ff38f2f
SheerSt <8891626+SheerSt@users.noreply.github.com>
2021-09-24 12:22:33 -0500

    beta.5 assets

---
+++ pokemon/credited/pokemon/riolu/anim.asm
@@ -0,0 +1,9 @@
+	frame 1, 04
+	frame 2, 04
+	setrepeat 4
+	frame 3, 03
+	frame 4, 04
+	dorepeat 3
+	frame 2, 05
+	frame 1, 05
+	endanim
---
+++ pokemon/credited/pokemon/volbeat/anim.asm
@@ -0,0 +1,10 @@
+	frame 1, 04
+	frame 2, 08
+	frame 1, 04
+	frame 2, 12
+	frame 3, 12
+	setrepeat 2
+	frame 4, 06
+	frame 3, 06
+	dorepeat 6
+	endanim

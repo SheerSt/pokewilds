b678cd4016d7f89067efe0975d46bb7ba7848fba
SheerSt <evandefend@yahoo.com>
2021-10-01 18:38:06 -0500

    v0.8 bugfixes

--- pokemon/credited/evos_attacks.asm
+++ pokemon/credited/evos_attacks.asm
@@ -17999,3 +17999,26 @@ CalyrexEvosAttacks:
 	db 88, FUTURE_SIGHT
 	db 0 ; no more level-up moves
 
+AmarowakEvosAttacks:
+	db EVOLVE_LEVEL, 33, CAMERUPT
+	db 0 ; no more evolutions
+	db 1, DOUBLE_EDGE
+	db 1, FALSE_SWIPE
+	db 1, FIRE_SPIN
+	db 1, FOCUS_ENERGY
+	db 1, GROWL
+	db 1, HEADBUTT
+	db 1, MUD_SLAP
+	db 1, RETALIATE
+	db 1, TAIL_WHIP
+	db 12, FLAME_WHEEL
+	db 16, HEX
+	db 20, FLING
+	db 24, STOMPING_TANTRUM
+	db 31, BONE_RUSH
+	db 36, WILL_O_WISP
+	db 42, ENDEAVOR
+	db 48, BONEMERANG
+	db 54, THRASH
+	db 60, FLARE_BLITZ
+	db 0 ; no more level-up moves

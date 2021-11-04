de16aec03dadc12f294ccdc5f4cfe01632e86ef4
SheerSt <8891626+SheerSt@users.noreply.github.com>
2021-11-03 21:06:13 -0500

    add diancie, komala, pyukumuku, durant, indeedee

---
+++ pokemon/credited/pokemon/diancie/anim.asm
@@ -0,0 +1,14 @@
+  frame 0, 06
+  frame 1, 06
+  frame 2, 12
+  frame 3, 06
+  frame 4, 06
+  frame 5, 06
+  frame 6, 15
+  frame 7, 06
+  frame 8, 06
+  frame 9, 06
+  frame 10, 06
+  frame 11, 06
+  frame 12, 06
+	endanim

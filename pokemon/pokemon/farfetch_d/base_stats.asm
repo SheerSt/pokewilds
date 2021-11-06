e6c2f7c61fa1ecbe69abac045b958b6b1e40074a
SheerSt <8891626+SheerSt@users.noreply.github.com>
2021-11-06 15:43:14 -0500

    move base_stats and wilds_data to pokemon dirs

---
+++ pokemon/pokemon/farfetch_d/base_stats.asm
@@ -0,0 +1,21 @@
+	db FARFETCH_D ; 083
+
+	db  52,  65,  55,  60,  58,  62
+	;   hp  atk  def  spd  sat  sdf
+
+	db NORMAL, FLYING ; type
+	db 45 ; catch rate
+	db 94 ; base exp
+	db NO_ITEM, STICK ; items
+	db GENDER_F50 ; gender ratio
+	db 100 ; unknown 1
+	db 20 ; step cycles to hatch
+	db 5 ; unknown 2
+	INCBIN "gfx/pokemon/farfetch_d/front.dimensions"
+	db 0, 0, 0, 0 ; padding
+	db GROWTH_MEDIUM_FAST ; growth rate
+	dn EGG_FLYING, EGG_GROUND ; egg groups
+
+	; tm/hm learnset
+	tmhm HEADBUTT, CURSE, TOXIC, PSYCH_UP, HIDDEN_POWER, SUNNY_DAY, SNORE, PROTECT, ENDURE, FRUSTRATION, IRON_TAIL, RETURN, MUD_SLAP, DOUBLE_TEAM, SWAGGER, SLEEP_TALK, SWIFT, DETECT, REST, ATTRACT, THIEF, STEEL_WING, CUT, FLY
+	; end

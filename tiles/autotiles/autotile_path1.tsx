15b82e9fac141164c7797acd3c56cde963921c84
Maya Oliveira <mayaseii.dev@gmail.com>
2022-02-27 23:29:49 +0000

    Autotiles.

---
+++ tiles/autotiles/autotile_path1.tsx
@@ -0,0 +1,19 @@
+<tileset name="autotile_path1">
+
+    <image source="tiles/autotiles/autotile_path1.png"/>
+
+    <properties>
+
+        <property name="isSolid" type="bool" value="false"/>
+        <property name="isLedge" type="bool" value="false"/>
+        <property name="isWater" type="bool" value="false"/>
+        <property name="isGrass" type="bool" value="false"/>
+        <property name="isTree" type="bool" value="false"/>
+        <property name="isSmashable" type="bool" value="false"/>
+        <property name="isCuttable" type="bool" value="false"/>
+        <property name="isHeadbuttable" type="bool" value="false"/>
+        <property name="isAutoTile" type="bool" value="true"/>
+
+    </properties>
+ 
+</tileset>

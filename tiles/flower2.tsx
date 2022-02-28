d40e21dcb54fec7cabfc069bb855fe188b86c738
Maya Oliveira <mayaseii.dev@gmail.com>
2022-02-28 17:27:44 +0000

    Added flower animations.

---
+++ tiles/flower2.tsx
@@ -0,0 +1,28 @@
+<tileset name="flower2">
+
+    <image source="tiles/flower2.png"/>
+
+    <properties>
+
+        <property name="isSolid" type="bool" value="false"/>
+        <property name="isLedge" type="bool" value="false"/>
+        <property name="isWater" type="bool" value="false"/>
+        <property name="isGrass" type="bool" value="false"/>
+        <property name="isTree" type="bool" value="false"/>
+        <property name="isSmashable" type="bool" value="false"/>
+        <property name="isCuttable" type="bool" value="false"/>
+        <property name="isHeadbuttable" type="bool" value="false"/>
+        <property name="isDiggable" type="bool" value="true"/>
+        <property name="isOverlay" type="bool" value="false"/>
+        <property name="isAutoTile" type="bool" value="false"/>
+
+    </properties>
+
+    <animation>
+
+        <frame tileid="0" duration="2"></frame>
+        <frame tileid="1" duration="2"></frame>
+
+    </animation>
+ 
+</tileset>

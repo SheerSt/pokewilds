9bcf721b2de25108a134864ad69989b28fabe56a
Maya Oliveira <mayaseii.dev@gmail.com>
2022-02-28 21:40:48 +0000

    Added more trees.

--- tiles/tree4.tsx
+++ tiles/tree4.tsx
@@ -1,6 +1,6 @@
-<tileset name="torch" width="16" height="20">
+<tileset name="tree4" width="16" height="32">
 
-    <image source="tiles/torch.png"/>
+    <image source="tiles/tree4.png"/>
 
     <properties>
 
@@ -17,12 +17,5 @@
         <property name="isAutoTile" type="bool" value="false"/>
 
     </properties>
-
-    <animation>
-
-        <frame tileid="0" x="0" y="0" duration="15"></frame>
-        <frame tileid="1" x="16" y="0" duration="15"></frame>
-
-    </animation>
  
 </tileset>

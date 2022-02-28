71da105497095a0cd3defc2ba4b919b22351201a
Maya Oliveira <mayaseii.dev@gmail.com>
2022-02-28 20:16:29 +0000

    Testing trees.

---
+++ tiles/tree2.tsx
@@ -0,0 +1,21 @@
+<tileset name="tree2" width="16" height="32">
+
+    <image source="tiles/tree2.png"/>
+
+    <properties>
+
+        <property name="isSolid" type="bool" value="false"/>
+        <property name="isLedge" type="bool" value="false"/>
+        <property name="isWater" type="bool" value="false"/>
+        <property name="isGrass" type="bool" value="false"/>
+        <property name="isTree" type="bool" value="false"/>
+        <property name="isSmashable" type="bool" value="false"/>
+        <property name="isCuttable" type="bool" value="false"/>
+        <property name="isHeadbuttable" type="bool" value="false"/>
+        <property name="isDiggable" type="bool" value="true"/>
+        <property name="isOverlay" type="bool" value="false"/>
+        <property name="isAutoTile" type="bool" value="false"/>
+
+    </properties>
+ 
+</tileset>
